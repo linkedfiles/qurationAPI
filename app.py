89ce16112ba44e072f9fb4b3355668fa73696d69
linkedfiles <34315418+linkedfiles@users.noreply.github.com>
2023-07-03 10:04:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from http import HTTPStatus
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 import main
 
 app = Flask(__name__)
 
-@app.route('/')
+
+data = {
+    "list" : [{ "TEXT": '가', "RANK": 2, "SCORE" : 3 }, { "TEXT": '나', "RANK": 1, "SCORE" : 2 }, { "TEXT": '다', "RANK": 3, "SCORE" : 1 } ]
+}
+
+@app.route('/get', methods=['GET'])
 def index():
-    return main.extract_keyworld()
+    return jsonify({"data": data, "status": HTTPStatus.OK})
+
+@app.route('/getlank', methods=['POST'])
+def post():
+    #params = request.get_json()
+    #print(params)
+    #return jsonify({"TEXT": main.extract_keyworld(), "status": HTTPStatus.OK})
+    return jsonify({"data": data, "status": HTTPStatus.OK})
 
 
-
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
